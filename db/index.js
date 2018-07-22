5f400884bf896d1114c3a5dddefc4dceb07f0c80
David Acconcio <dacconcio@gmail.com>
2018-07-22 16:55:23 -0400

    updated server url for heroku

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const pg = require('pg');
 const { Client } = require('pg')
 
-const client = new Client('postgres://localhost/wnews')
+const client = new Client(process.env.DATABASE_URL);
 client.connect();
 
 
